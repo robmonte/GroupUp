4aa4461662143e1791ab596c3232c6ba4bbbbfe5
Victor Yang <Max@wireless-10-147-13-17.public.utexas.edu>
2017-03-22 20:08:51 -0500

    Made map not fixed at current location

--- GroupUp/MapViewController.swift
+++ GroupUp/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
+    var firstLocUpdate = true
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,12 +35,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate{
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
+        if firstLocUpdate {
+            let location = locations.last
         
-        let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         
-        self.mapView.setRegion(region, animated: true)
+            self.mapView.setRegion(region, animated: true)
+            firstLocUpdate = false
+        }
     }
     
 
